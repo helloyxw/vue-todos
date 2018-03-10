4847b225b3ac0d885a109e6550defe65aaae8770
helloyxw <helloyxw@163.com>
2018-03-11 00:06:29 +0800

    增加时间戳

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <ul class="todos">  
        <li v-for="(todo, index) in todos" :id="index" :class="{'checked': todo.done}">
            <label>{{ index + 1 }}.{{ todo.value }}</label>
+           <time>{{ todo.created | date }}</time>
        </li>
     </ul>
   </div>
@@ -11,17 +12,24 @@
 
 <script>
 import './assets/site.less';
-
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
 export default {
   name: 'app',
   data () {
     return {
       title: "vue-todos",
       todos: [
-          { value: "阅读一本关于前端开发的书", done: false },
-          { value: "补充范例代码", done: true },
-          { value: "写心得啦", done: false }
+          { value: "阅读一本关于前端开发的书", done: false, created: Date.now() },
+          { value: "补充范例代码", done: true, created: Date.now() + 300000 },
+          { value: "写心得啦", done: false, created: Date.now() - 300000 }
       ]
+    }
+  },
+  filters: {
+    date(val) {
+        return moment(val).calendar();
     }
   }
 }
